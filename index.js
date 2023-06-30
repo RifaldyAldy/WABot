4268e8ff43ee2964904bcc8d571e08242a584366
RifaldyAldy <112386802+RifaldyAldy@users.noreply.github.com>
2023-06-30 15:50:03 +0700

    Update chat

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Menambahkan dependencies
 const { default: makeWASocket, DisconnectReason, useMultiFileAuthState } = require('@whiskeysockets/baileys');
 const Boom = require('@hapi/boom');
+const GPT = require('./openai');
 
 // fungsi utama WA BOT
 async function connectToWhatsapp() {
@@ -34,12 +35,58 @@ async function connectToWhatsapp() {
   await sock.ev.on('creds.update', saveCreds);
 
   // Fungsi untuk memantau pesan masuk
-  sock.ev.on('messages.upsert', ({ messages, type }) => {
+  sock.ev.on('messages.upsert', async ({ messages, type }) => {
     console.log(`Tipe pesan : ${type}`);
-    console.log(messages);
+    const number = messages[0].key.remoteJid;
+    if (type === 'notify' && !messages[0].key.fromMe) {
+      try {
+        let selainPesan = false;
+        console.log(messages);
+        let chat = messages[0].message.conversation;
+        if (!chat) {
+          chat = messages[0].message.extendedTextMessage.text;
+        }
+        const ambilRequestChat = chat.split(' ').slice(1).join(' ').toString();
+        const ambilRequest = ambilRequestChat.split(' ')[0];
+
+        //membuat variabel untuk mengecek apakah pesan dari group dan mention bot
+        const isMessageGroup = number.includes('@g.us');
+        const isMessageMentionBot = chat.includes('@628388995241');
+
+        console.log(number);
+        console.log(chat);
+        if (chat.toLowerCase() === 'ping') {
+          const send = await sock.sendMessage(number, { text: 'Halo, selamat datang\nBot Berfungsi dengan baik!' }, { quoted: messages[0] }, 2000);
+          selainPesan = true;
+        }
+
+        //logic jika mention bot saja
+        if (isMessageGroup && isMessageMentionBot && ambilRequestChat.includes('/nanya')) {
+          await sock.sendMessage(number, { text: 'Sebentar sedang mikir...' }, { quoted: messages[0] }, 2000);
+          const req = await GPT(ambilRequestChat);
+          await sock.sendMessage(number, { text: req }, { quoted: messages[0] }, 2000);
+          selainPesan = true;
+          // console.log(ambilRequestChat);
+        }
+        if (chat.includes('/nanya')) {
+          await sock.sendMessage(number, { text: 'Sebentar sedang mikir...' }, { quoted: messages[0] }, 2000);
+          console.log('isi pesan nya', ambilRequestChat);
+          const req = await GPT(ambilRequestChat);
+          await sock.sendMessage(number, { text: req }, { quoted: messages[0] }, 2000);
+          console.log(ambilRequestChat);
+          selainPesan = true;
+        }
+        if (selainPesan === false) {
+          await sock.sendMessage(number, { text: `Maaf, pesan anda tidak saya kenali,\nuntuk membuat pertanyaan silahkan pakai kunci \n"/nanya (pertanyaan anda)"` });
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
   });
 }
 
+// menjalankan Whatsapp bot
 connectToWhatsapp().catch((error) => {
   console.log(error);
 });
